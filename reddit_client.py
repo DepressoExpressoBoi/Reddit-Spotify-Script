bdacc0b20a4adcc52d06d74d3dca2e7c3ce31404
DepressoExpressoBoi <saidajomontano@gmail.com>
2020-06-29 11:40:30 -0600

    yay

--- reddit_client.py
+++ reddit_client.py
@@ -1,23 +1,11 @@
 import praw
-
+    
 clientId = 'cGmfkXximckDKg'
 clientSecret = 'ht8lTNQewNRvJaiF52IrDsn4T5g'
 userAgent = 'ListenToThisAgent'
 
 reddit = praw.Reddit(clientId, clientSecret, userAgent)
+subreddit = reddit.subreddit('listentothis')
 
-class redditClient(object):
-    def __init__(self):
-        pass
-
-    def getSubbreddit(self):
-        self.subreddit = reddit.subreddit('listentothis')
-
-    def getGenres(self):
-        pass
-
-    def getSongs(self):
-        pass
-
-    def getArtists(self):
-        pass
+for submission in subreddit.hot(limit = 1):
+    print(submission.title) 
