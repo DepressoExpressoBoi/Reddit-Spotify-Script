9d87cb04fc9e2421dfcdb780e64c157010cd4439
DepressoExpressoBoi <saidajomontano@gmail.com>
2020-06-29 12:09:39 -0600

    yaay

--- reddit_client.py
+++ reddit_client.py
@@ -1,11 +1,24 @@
 import praw
-    
+
 clientId = 'cGmfkXximckDKg'
 clientSecret = 'ht8lTNQewNRvJaiF52IrDsn4T5g'
 userAgent = 'ListenToThisAgent'
+lim = 100
 
-reddit = praw.Reddit(clientId, clientSecret, userAgent)
+reddit = praw.Reddit(client_id = clientId, 
+                     client_secret = clientSecret, 
+                     user_agent = userAgent)
+
 subreddit = reddit.subreddit('listentothis')
+submissions = []
+songs = []
 
-for submission in subreddit.hot(limit = 1):
-    print(submission.title) 
+for submission in subreddit.hot(limit = lim):
+    if not submission.stickied:
+        submissions.append(submission.title) 
+
+for x in range(len(submissions)):
+    if 'Indie' in submissions[x]:
+        songs.append(submissions[x])
+
+print(songs[0].split(' - '))
